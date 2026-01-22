98757045fce1d095eb23e1b3f851793263f14208
EMC-developer01 <a.geethas@emircorp.com>
2026-01-22 10:52:15 +0530

    docker updated completing

--- serverSide/backend/config/db.js
+++ serverSide/backend/config/db.js
@@ -1,22 +1,37 @@
 import mongoose from "mongoose";
 
 
+// const connectDB = async () => {
+//   try {
+//     const isDocker = process.env.IS_DOCKER === "true";
+
+//     const mongoHost = isDocker ? "host.docker.internal" : "127.0.0.1";
+//     const mongoURI = process.env.MONGO_URI || `mongodb://${mongoHost}:27017/epudubackend`
+
+//     // const conn = await mongoose.connect(`mongodb://${mongoHost}:27017/epudubackend`);
+
+//     const conn = await mongoose.connect(mongoURI);
+
+//     // const conn = await mongoose.connect(process.env.MONGO_URI, {
+//     //   useNewUrlParser: true,
+//     //   useUnifiedTopology: true,
+//     // });
+//     console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+//   } catch (error) {
+//     console.error(`❌ MongoDB Connection Error: ${error.message}`);
+//     process.exit(1);
+//   }
+// };
+
+// export default connectDB;
+
+// import mongoose from "mongoose";
+
 const connectDB = async () => {
   try {
-    const isDocker = process.env.IS_DOCKER === "true";
+    const conn = await mongoose.connect(process.env.MONGO_URI);
 
-    const mongoHost = isDocker ? "host.docker.internal" : "127.0.0.1";
-    const mongoURI = process.env.MONGO_URI || `mongodb://${mongoHost}:27017/epudubackend`
-
-    // const conn = await mongoose.connect(`mongodb://${mongoHost}:27017/epudubackend`);
-
-    const conn = await mongoose.connect(mongoURI);
-
-    // const conn = await mongoose.connect(process.env.MONGO_URI, {
-    //   useNewUrlParser: true,
-    //   useUnifiedTopology: true,
-    // });
-    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+    console.log(`✅ MongoDB Atlas Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`❌ MongoDB Connection Error: ${error.message}`);
     process.exit(1);
