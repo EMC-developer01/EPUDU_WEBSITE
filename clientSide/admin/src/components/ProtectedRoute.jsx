422639b2addabb04a9479946bfedd8db7f255a37
EMC-developer01 <a.geethas@emircorp.com>
2026-02-02 10:44:30 +0530

    final version docker

--- clientSide/admin/src/components/ProtectedRoute.jsx
+++ clientSide/admin/src/components/ProtectedRoute.jsx
@@ -3,6 +3,6 @@ import { Navigate, useLocation } from "react-router-dom";
 
 export default function ProtectedRoute({ children }) {
     const token = localStorage.getItem("adminToken");
-    return token ? children : <Navigate to="/" replace />;
+    return token ? children : <Navigate to="/login" replace />;
 }
 
