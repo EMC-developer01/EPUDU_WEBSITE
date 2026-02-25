e87463b6380838bd109dbf27713e691d09b56a5b
EMC-developer01 <a.geethas@emircorp.com>
2026-02-25 18:35:27 +0530

    change 58

--- clientSide/admin/vite.config.js
+++ clientSide/admin/vite.config.js
@@ -17,7 +17,9 @@ export default defineConfig({
   },
   server: {
     host: true,
-    port: 5174
+    port: 5174,
+    allowedHosts : ['epudu.com']
+
   }
 
 })
--- clientSide/vendor/vite.config.js
+++ clientSide/vendor/vite.config.js
@@ -18,6 +18,8 @@ export default defineConfig({
   },
   server: {
     host: true,
-    port: 5175
+    port: 5175,
+    allowedHosts: ['epudu.com']
+
   }
 })
