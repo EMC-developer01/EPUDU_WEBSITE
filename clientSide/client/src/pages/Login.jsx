f0a97b2ae52fff4c956a611344edb4cc0d35afc9
EMC-developer01 <a.geethas@emircorp.com>
2026-02-26 15:03:10 +0530

    change 59

--- clientSide/client/src/pages/Login.jsx
+++ clientSide/client/src/pages/Login.jsx
@@ -29,7 +29,7 @@ const Login = ({ onClose }) => {
     // setStep("otp");
 
     try {
-      const res = await fetch(`${API_URL}/api/send-otp`, {
+      const res = await fetch(`${API_URL}/send-otp`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ mobile }),
