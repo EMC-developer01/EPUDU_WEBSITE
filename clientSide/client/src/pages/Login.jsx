98ff7c1b1388f9ebeb07435592c666396111e092
EMC-developer01 <a.geethas@emircorp.com>
2026-03-03 15:28:19 +0530

    change 80

--- clientSide/client/src/pages/Login.jsx
+++ clientSide/client/src/pages/Login.jsx
@@ -198,7 +198,7 @@ const Login = ({ onClose }) => {
             />
             <button
               type="submit"
-              className="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition"
+              className="w-full bg-blue-600 text-white text-white px-5 py-2 rounded-lg hover:bg-indigo-700"
             >
               Send OTP
             </button>
