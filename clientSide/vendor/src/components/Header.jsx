6d8aa6d5d5b03cd4fbd5605f517cff22c58ce9ef
EMC-developer01 <a.geethas@emircorp.com>
2026-03-07 15:16:37 +0530

    change 133

--- clientSide/vendor/src/components/Header.jsx
+++ clientSide/vendor/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import logo from "../assets/logo.png";
+import logo from "../assets/logo-try.png";
 
 export default function Header() {
     const [menuOpen, setMenuOpen] = useState(false);
