14105338d557e72561fb0a1ae4f0638548fa449e
EMC-developer01 <a.geethas@emircorp.com>
2026-03-07 10:59:46 +0530

    change 127

--- clientSide/vendor/src/pages/VendorRegistration.jsx
+++ clientSide/vendor/src/pages/VendorRegistration.jsx
@@ -10,7 +10,7 @@ export default function VendorRegistration() {
 
     const [vendorSignature, setVendorSignature] = useState(null);
     const [vendorSignaturePreview, setVendorSignaturePreview] = useState(null);
-    API_URL = import.meta.env.VITE_API_URL;
+    let API_URL = import.meta.env.VITE_API_URL;
     const MEDIA_URL = import.meta.env.VITE_MEDIA_URL;
 
 
