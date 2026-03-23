a0d6cc19b2e34b2e128f7ed5dc9c2acecb1df123
EMC-developer01 <a.geethas@emircorp.com>
2026-03-23 12:04:47 +0530

    change 261

--- serverSide/backend/scripts/uploadToS3.js
+++ serverSide/backend/scripts/uploadToS3.js
@@ -1,11 +1,21 @@
 import AWS from "aws-sdk";
 import multer from "multer";
 import mime from "mime-types";
+import { S3Client } from "@aws-sdk/client-s3";
 
-const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+
+// const s3 = new AWS.S3({
+//   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+//   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+//   region: process.env.AWS_REGION,
+// });
+
+export const s3 = new S3Client({
   region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
 });
 
 const upload = multer({
